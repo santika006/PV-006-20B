a8b38a4687e671b16d5381f6b6711f80c2279305
santika006 <72698209+santika006@users.noreply.github.com>
2022-03-30 22:52:48 +0700

    Add files via upload

---
+++ Praktikum 3 No 2.py
@@ -0,0 +1,7 @@
+
+desimal = int(input('Masukkan Bilangan Desimal = '))
+
+biner = bin(desimal) .replace("0b","")
+oktal = oct(desimal) .replace("0o","")
+hexa = hex(desimal) .replace("0x","")
+print(biner,oktal,hexa)
